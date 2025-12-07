503aabc9a81a63f2ff716a74ef7469a0687c8d10
ellis <lsp1@hotmail.com>
2025-12-07 15:38:21 -0700

    google auth setup

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import fs from "node:fs/promises"
+import { google } from "googleapis";
 import readOgRomBinaryGameState from "./gameStateParsing/game-state/read-og-rom-game-state.js"
+
+/////////////////////
+// google auth setup
+/////////////////////
+
+const getServiceAccountJSON = await fs.readFile("./serviceKeys.json");
+const serviceAccount = JSON.parse(getServiceAccountJSON)
+
+// Initialize the Sheets API client in the main thread
+const auth = new google.auth.GoogleAuth({
+  credentials: serviceAccount,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+const sheets = google.sheets({ version: "v4", auth });
+
+const spreadSheetId = process.env.spreadSheetId
+
+/////////////////
+// read game data
+/////////////////
 
 const buffer = await fs.readFile("./NHL_95.state30")
 
@@ -9,5 +30,3 @@ const gameFileBuffer = buffer.buffer.slice(
 );
 
 const gameData = await readOgRomBinaryGameState(gameFileBuffer)
-
-console.log(gameData.data)
